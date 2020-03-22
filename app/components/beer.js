3538bb82505c5e5c76391357d29fb0fec2344ee8
Doug Banville <doug.banville@gmail.com>
2020-03-22 18:54:34 +0000

    Demo ready

--- app/components/beer.js
+++
@@ -1,8 +0,0 @@
-import Component from '@glimmer/component';
-
-export default class BeerComponent extends Component {
-
-  bgImage = el=>{
-    el.setAttribute("style", `background-image: url('${this.args.beer.image_url}');`);
-  }
-}
